daec98c8346ede6a34c24654d424d78cb84191c8
JeO <jeo2k1@gmail.com>
2024-03-15 16:08:31 -0300

    22 consulta de datos I

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Author, Entry
 
 # Create your views here.
+def queries(request):
+    # obtener todos los elementos
+    authors = Author.objects.all()
+    return render(request, 'post/queries.html',{'authors':authors})
