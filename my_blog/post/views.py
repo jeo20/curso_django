c39dbe85298022b1f22595d378e4a74921b131e7
JeO <jeo2k1@gmail.com>
2024-03-15 16:09:44 -0300

    obtener todos los autores

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -4,5 +4,5 @@ from .models import Author, Entry
 # Create your views here.
 def queries(request):
     # obtener todos los elementos
-    authors = Author.objects.all()
-    return render(request, 'post/queries.html',{'authors':authors})
+    authors = Author.objects.all() # SELECT * FROM authors;
+    return render(request, 'post/queries.html',{'authors':authors}) #  {'authors':authors} -> para pasar variables a la plantilla
