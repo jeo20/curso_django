569ef1684ab09ec1b8a8b4c1a3c911faa08e6288
JeO <jeo2k1@gmail.com>
2024-03-18 18:53:15 -0300

    clase 22 hasta filtro por un objeto unico

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -5,4 +5,11 @@ from .models import Author, Entry
 def queries(request):
     # obtener todos los elementos
     authors = Author.objects.all() # SELECT * FROM authors;
-    return render(request, 'post/queries.html',{'authors':authors}) #  {'authors':authors} -> para pasar variables a la plantilla
+    
+    # obtener datos filtrados por condicion
+    filtered = Author.objects.filter(email= 'lynn58@example.net')  #SELECT * FROM authors WHERE email='lynn58@example.net';
+    
+    # obtener un objeto unico(filtrado)
+    author = Author.objects.get(id = 1)
+    
+    return render(request, 'post/queries.html',{'authors':authors, 'filtered': filtered, 'author': author}) #  {'authors':authors} -> para pasar variables a la plantilla
