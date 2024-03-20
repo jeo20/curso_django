54bb9db1b59f820ff4668ab6c0cfc643c847af5e
JeO <jeo2k1@gmail.com>
2024-03-20 15:49:03 -0300

    22 consulta de datos I

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -10,6 +10,15 @@ def queries(request):
     filtered = Author.objects.filter(email= 'lynn58@example.net')  #SELECT * FROM authors WHERE email='lynn58@example.net';
     
     # obtener un objeto unico(filtrado)
-    author = Author.objects.get(id = 1)
+    author = Author.objects.get(id = 1) # SELECT * FROM authors WHERE id=1;
     
-    return render(request, 'post/queries.html',{'authors':authors, 'filtered': filtered, 'author': author}) #  {'authors':authors} -> para pasar variables a la plantilla
+    # obtener los 10 primeros elementos
+    limits = Author.objects.all()[:10] # SELECT * FROM authors LIMIT 10;
+    
+    # obtener 5 elementos saltando los primeros 5
+    offsets = Author.objects.all()[5:10] # SELECT * FROM authors LIMIT 5 OFFSET 5;
+        
+    # Retorna render de la request al template  "queries.html" con el contexto definido entre llaves {} de cada metodo
+    return render(request, 'post/queries.html',{'authors':authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets})
+
+    
