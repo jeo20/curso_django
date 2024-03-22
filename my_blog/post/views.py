e75b197de0a853fc0d1f3c11263e616e81d16212
JeO <jeo2k1@gmail.com>
2024-03-22 15:41:44 -0300

    23 consulta de datos II

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -7,7 +7,7 @@ def queries(request):
     authors = Author.objects.all() # SELECT * FROM authors;
     
     # obtener datos filtrados por condicion
-    filtered = Author.objects.filter(email= 'lynn58@example.net')  #SELECT * FROM authors WHERE email='lynn58@example.net';
+    #filtered = Author.objects.filter(email= 'lynn58@example.net')  #SELECT * FROM authors WHERE email='lynn58@example.net';
     
     # obtener un objeto unico(filtrado)
     author = Author.objects.get(id = 1) # SELECT * FROM authors WHERE id=1;
@@ -17,8 +17,15 @@ def queries(request):
     
     # obtener 5 elementos saltando los primeros 5
     offsets = Author.objects.all()[5:10] # SELECT * FROM authors LIMIT 5 OFFSET 5;
-        
-    # Retorna render de la request al template  "queries.html" con el contexto definido entre llaves {} de cada metodo
-    return render(request, 'post/queries.html',{'authors':authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets})
-
     
+    # obtener todos los elementos ordenados
+    orders = Author.objects.all().order_by(('email')) # SELECT * FROM authors ORDER BY  email;
+
+    # obtener datos filtrados por condicion
+    filtered = Author.objects.filter(id__lte=15) #SELECT * FROM authors WHERE id<=15;
+    
+    # obtener todos los autores que contienen en su nombre la palabra yes
+    contains = Author.objects.filter(name__contains="yes")
+    
+    # Retorna render de la request al template  "queries.html" con el contexto definido entre llaves {} de cada metodo
+    return render(request, 'post/queries.html',{'authors':authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets': offsets, 'orders': orders, 'contains': contains})
