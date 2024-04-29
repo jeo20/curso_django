1bb578ffa22163cefe427de140bcb6ad8024c25d
JeO <jeo2k1@gmail.com>
2024-04-29 20:16:40 -0300

    33 formularios a traves de clases

--- formularios_django/formularios_django/views.py
+++ formularios_django/formularios_django/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from .forms import ComentForm
 
 def form(request):
-    coment_form = ComentForm()
+    coment_form = ComentForm({'name': 'Jorge', 'url': 'http://jeo.com','comment':'comentario jeo'}) # crea el formulario con los datos iniciales
     return render(request, 'form.html',{'coment_form':coment_form})
 
-def goal(request):
-    if request.method != 'GET':
-        return HttpResponse("Metodo no permitido")
+def goal(request): # funcion que recibe el formulario
+    if request.method != 'POST':
+        return HttpResponse("Metodo no permitido") # devuelve "metodo no permitido" cuando es distinto de GET
     
-    return HttpResponse("Recibido")
+    return HttpResponse(request.POST['name']) # devuelve el nombre del campo name del formulario
