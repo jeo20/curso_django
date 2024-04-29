293441a2bde6479dccece81593c398c8f040c893
JeO <jeo2k1@gmail.com>
2024-04-28 23:22:48 -0300

    32 formulario html post

--- formularios_html/formularios_html/views.py
+++ formularios_html/formularios_html/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def form(request):
+def getform(request):
     return render(request, 'form.html', {})
 
-def goal(request):
+def getgoal(request):
     if request.method != 'GET': # si el metodo no es GET o distinto de GET
-        return HttpResponse("El metodo POST no esta soportado para esta ruta")        
-        
+        return HttpResponse("El metodo POST no esta soportado para esta ruta")                
     name = request.GET['name']
     return render(request, 'success.html', {'name':name})
+
+def postform(request):
+    return render(request, 'postform.html', {})
+
+def postgoal(request):
+    if request.method != 'POST':
+        return HttpResponse("El metodo POST no esta soportado para esta ruta")
+    info = request.POST['info']
+    return render(request, 'postsuccess.html', {'info':info})
