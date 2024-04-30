e1085fa2507df9165be9f8777f6e06e6676c6d34
JeO <jeo2k1@gmail.com>
2024-04-30 15:53:23 -0300

    34 widget en formularios

--- formularios_django/formularios_django/views.py
+++ formularios_django/formularios_django/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ComentForm
+from .forms import ComentForm, ContactForm
 
 def form(request):
     coment_form = ComentForm({'name': 'Jorge', 'url': 'http://jeo.com','comment':'comentario jeo'}) # crea el formulario con los datos iniciales
@@ -11,3 +11,7 @@ def goal(request): # funcion que recibe el formulario
         return HttpResponse("Metodo no permitido") # devuelve "metodo no permitido" cuando es distinto de GET
     
     return HttpResponse(request.POST['name']) # devuelve el nombre del campo name del formulario
+
+def widget(request):# vista widget, recibe la request del formulario y la muestra en una pagina html
+    form = ContactForm
+    return render(request, 'widget.html', {'form': form})
