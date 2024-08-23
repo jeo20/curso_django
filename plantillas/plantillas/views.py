7bfcea8748c928d895fa6accdf0c0965a8996c9a
JeO <jeo20@hotmail.com>
2024-08-23 14:31:48 -0300

    hasta 08 Uso de contextos - Curso Django - OpenBootcamp

--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render  # importar el modulo de renderizado
 
 
 def simple(request):
-    return render(request, "simple.html")  # Retornar la plantilla simple.html y necesita un diccionario vacio
+    return render(request, "simple.html", {})  # Retornar la plantilla simple.html, pasamos contexto vacio{}
+
+
+def dinamico(request, name):
+    categories = ['code','design','marketing','business'] 
+    # lista con las categorias posibles para mostrar en la pagina
+    
+    contexto = {'name': name, 'categories': categories} 
+    # creamos una variable contexto y guardamos un diccionario con el nombre como clave(name que recibimos de parametro) y lo pasamos al template
+    
+    return render(request, 'dinamico.html', contexto)  
+    # se le pasa al render el request y el nombre de la plantilla con el contexto
