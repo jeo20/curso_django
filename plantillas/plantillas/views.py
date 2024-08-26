8bdf45aef92558a0e31e257fd39dfbfcda81904d
JeO <jeo20@hotmail.com>
2024-08-26 09:41:47 -0300

    hasta 08 Uso de contextos - Curso Django - OpenBootcamp

--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -10,7 +10,8 @@ def dinamico(request, name):
     # lista con las categorias posibles para mostrar en la pagina
     
     contexto = {'name': name, 'categories': categories} 
-    # creamos una variable contexto y guardamos un diccionario con el nombre como clave(name que recibimos de parametro) y lo pasamos al template
+    # creamos una variable contexto y guardamos un diccionario con el nombre como clave(name que recibimos de parametro) y el arreglo categories,
+    # luego lo pasamos al template
     
     return render(request, 'dinamico.html', contexto)  
     # se le pasa al render el request y el nombre de la plantilla con el contexto
