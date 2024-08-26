a9a5f48011d9afc91b6d59d53773d90c5141335c
JeO <jeo20@hotmail.com>
2024-08-26 18:49:04 -0300

    17 modelos de datos - Curso Django - OpenBootcamp

--- modularizacion/comentarios/models.py
+++ modularizacion/comentarios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class comments(models.Model):    
+    name = models.CharField(max_length=255, null=False)
+    score = models.IntegerField(default=3)
+    comment = models.TextField(max_length=1000, null=False)
+    date = models.DateField(null=True)
+    signature = models.CharField(max_length=100, default="Firma")
+    
+    def __str__(self):
+        return self.name
